a642e7152eeb91f6e8dff2f702dd77061ea55ba2
Evan Lloyd <evan.u.lloyd@gmail.com>
2017-12-25 19:42:14 -0500

    Add bash script to install elixir 1.5.0

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+bash 'install elixir 1.5.0' do
+  user 'root'
+  code <<-EOC
+    wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+    sudo dpkg -i erlang-solutions_1.0_all.deb
+    sudo apt-get update
+    sudo apt-get install -y esl-erlang 20.0
+    sudo apt-get install -y elixir 1.5.0
+  EOC
+end
